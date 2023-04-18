a29b6b38ba9f36cb3db425a9018ee0becf527453
Javier Martinez <javier.martinezsa@sansano.usm.cl>
2023-04-18 17:58:33 -0400

    fix: fixed import in index js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import VercelLogo from "@/public/vercel.svg"
 import NextLogo from "@/public/next.svg"
 import { Inter } from 'next/font/google'
-import styles from '@/src/styles/Home.module.css'
+import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
