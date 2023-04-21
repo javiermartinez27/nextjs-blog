58e6d7cf3122c02e8b50b9228aadc8b223fdc683
Javier Martinez <javier.martinezsa@sansano.usm.cl>
2023-04-21 13:45:35 -0400

    feat: addeed layout, typescript and clsx

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+function NavBar() {
+  return (
+    <div className="grid w-full justify-items-center">
+      <div className="rounded shadow bg-white w-11/12 fixed p-4 m-2">This is a bar</div>
+    </div>
+  )
+}
+
+export default NavBar;
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-full h-screen">
+      <div className="m-4 text-xl font-medium">Este es mi blog! :D</div>
+    </div>
+  )
+}
